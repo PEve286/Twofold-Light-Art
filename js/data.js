3b59a878b7c1f1d603d169ae841862bce9c46e74
Aiman <65372518+Aiman7756@users.noreply.github.com>
2024-06-05 14:52:17 -0700

    template files

---
+++ js/data.js
@@ -0,0 +1,15 @@
+var data = {
+	images: [
+		{
+			src: [""],
+			thumbnail: "",
+			title: "",
+			artist: "",
+			artist_url: null,
+			art_url: null,
+			desc: "",
+			date_str: "",
+			tags: [],
+			hidden: true
+		}
+		]}
