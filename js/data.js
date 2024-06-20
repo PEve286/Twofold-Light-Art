7246874d22fd05214bdce24a52e5394cd5401596
PEve286 <162827930+PEve286@users.noreply.github.com>
2024-06-19 20:48:57 -0500

    index update

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
 var data = {
 	images: [
 		{
-			src: [""],
-			thumbnail: "",
-			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
-			desc: "",
-			date_str: "",
+			src: ["https://pbs.twimg.com/media/FOaKLUOXoAchmgJ?format=jpg&name=4096x4096"],
+			thumbnail: "https://pbs.twimg.com/media/FOaKLUOXoAchmgJ?format=jpg&name=4096x4096",
+			title: "Leif Reference",
+			artist: "Dt75Art",
+			artist_url: "https://twitter.com/Dt75Art",
+			art_url: "https://x.com/Dt75Art/status/1506043460914233352",
+			desc: "Leif's appearance in Twofold Light",
+			date_str: "March 21, 2022",
 			tags: [],
-			hidden: true
+			hidden: false
 		}
 		]}
